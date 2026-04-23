e69e0cedcc1365a488b054ddc057526c4f6cc023
everestp <PAUDELEVEREST09@GMAIL.COM>
2026-04-24 01:47:01 +0545

    feat : withdraw endpoint

--- server/src/routers/v1/index.routes.ts
+++ server/src/routers/v1/index.routes.ts
@@ -3,6 +3,7 @@ import pingRouter from './ping.routes';
 import authRouter from './auth.routes';
 import nodeRouter from './node.routes';
 import nodeMapRouter from './nodeMap.routes';
+import withdrawRouter from './withdraw.routes';
 
 const v1Router = express.Router();
 
@@ -12,5 +13,6 @@ v1Router.use('/ping',  pingRouter);
 v1Router.use('/auth',authRouter)
 v1Router.use('/node',nodeRouter)
 v1Router.use('/map',nodeMapRouter)
+v1Router.use('/withdraw',withdrawRouter)
 
 export default v1Router;
